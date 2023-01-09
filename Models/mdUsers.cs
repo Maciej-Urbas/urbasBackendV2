d83f1942122e90345a90fc383f0f1b48773e5e62
Maciej Urbas <maciej.urbas3@gmail.com>
2023-01-10 00:27:17 +0100

    :sparkles: Make mdUsers.cs file with MdUser model

---
+++ Models/mdUsers.cs
@@ -0,0 +1,10 @@
+namespace urbasBackendV2.Models;
+
+public class MdUser
+{
+    public long Id { get; set; }
+    
+    public string? Login { get; set; }
+
+    public string? Password { get; set; }
+}
